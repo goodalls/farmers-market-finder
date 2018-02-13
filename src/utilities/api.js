dc3946f25e211fd3fb1cb2cf27963342dcef4639
Jeff Goodall <goodallhj@gmail.com>
2018-02-13 10:20:04 -0700

    Adding generic fetch parse funcion in api

---
+++ src/utilities/api.js
@@ -0,0 +1,10 @@
+export const fetchParse = async(url) => {
+  try {
+    const initialFetch = await fetch(url);
+    const response = await initialFetch.json();
+    return response;
+  } catch (error) {
+    return 'fetchParse error:' + error;
+  }
+};
+
---
+++ src/utilities/api.test.js
@@ -0,0 +1,38 @@
+import * as api from './api';
+
+describe('fetchParse', () => {
+  beforeAll(() => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve([
+            {
+              results: [{}, {}]
+            }
+          ])
+      })
+    );
+    mockUrl = 'http://mock.com';
+  });
+
+  it('should call fetch with the expected params', () => {
+    api.fetchParse(mockUrl);
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl);
+  });
+
+  it('should return a parsed data object for a given URL', () => {
+    const expectedMovies = [{ results: [{}, {}] }];
+    expect(api.fetchParse(mockUrl)).resolves.toEqual(expectedMovies);
+  });
+
+  it('should error if the request is rejected', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.reject({
+        status: 404,
+        json: () => Promise.reject('fetchParse Err')
+      })
+    );
+    const error = await api.fetchParse(mockUrl);
+    expect(error).toEqual('fetchParse Err');
+  });
+});
