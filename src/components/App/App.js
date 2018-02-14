d1e8ef4d6f385683c6fbfde6d86c9c2f8bc6570f
Jeff Goodall <goodallhj@gmail.com>
2018-02-14 08:59:26 -0700

    initial fetch made
    Using the navigation.geolocation code to pull a list of markets

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,29 +4,34 @@ import * as api from '../../utilities/api';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      error: []
+      error: [],
+      position: {}
     };
   }
 
   componentDidMount() {
-    this.initialFetch()
+    this.initialFetch();
   }
 
-  initialFetch() {
-    const location = navigator.geolocation.getCurrentPosition(position => {
-      return position;
-    });
-
-  }
-  
-  render() {
-    return (
-      <div className="App">
-      Hello
-      </div>
+  async initialFetch() {
+    const location = await navigator.geolocation.getCurrentPosition(
+      response => {
+        const { latitude, longitude } = response.coords;
+        this.getNearbyMarkets(parseFloat(latitude), parseFloat(longitude));
+      }
     );
+  }
+
+  getNearbyMarkets = async (latitude, longitude) => {
+    const url = `http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=${latitude}&lng=${longitude}`;
+    const initial = await api.fetchParse(url);
+    console.log(initial)
+  };
+
+  render() {
+    return <div className="App">Hello</div>;
   }
 }
 
