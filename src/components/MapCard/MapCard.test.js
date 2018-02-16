8b3f07316a98adb840699eb2d17572560798798c
Jeff Goodall <goodallhj@gmail.com>
2018-02-16 16:52:20 -0700

    set up files for the card displays

---
+++ src/components/MapCard/MapCard.test.js
@@ -0,0 +1,7 @@
+import MapCard from './MapCard';
+
+describe('MAPCARD', () => {
+  it('should', () => {
+  
+  });
+});
---
+++ src/components/TextCard/TextCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TextCard.css';
+
+const TextCard = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default TextCard;
