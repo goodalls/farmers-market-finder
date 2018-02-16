868d532981891bc20564e67378fc517c0fd0b6b7
Jeff Goodall <goodallhj@gmail.com>
2018-02-15 18:59:37 -0700

    Adding location search in Control

--- src/components/Control/Control.js
+++ src/components/Control/Control.js
@@ -6,30 +6,56 @@ export class Control extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zip: ''
+      zip: '',
+      searchByZip: false,
+      searchByLocation: false
     };
   }
 
-  handleChange = (event) => {
-    const {value} = event.target;
-    
-    this.setState({zip: value});
-  }
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({ zip: value });
+  };
 
-  handleSubmit(event) {
+  handleSubmit = event => {
     event.preventDefault();
     console.log('handleSubmit');
-    //setState of the zip
-  }
-  render() {
+    this.setState({
+      searchByZip: true,
+      searchByLocation: false
+    });
+  };
 
+  handleCurrentLocation = event => {
+    this.setState({ 
+      searchByZip: false, 
+      searchByLocation: true 
+    });
+  };
+
+  render() {
     return (
       <section className="control">
+        <button onClick={this.handleCurrentLocation}>
+          Search by Current Location
+        </button>
+        <h4>
+          search for Farmers markets near you by entering your zip code below or
+          choosing to find markets close to your current location
+        </h4>
         {/* <NavLink to={'/sign-in'}>{Sign - In}</NavLink>
         <NavLink to={'/favorites'}>{Favorites}</NavLink> */}
         <form onSubmit={this.handleSubmit} className="form">
           <label htmlFor="zip" />
-          <input type="number" id="zip" maxLength="5" name="zip" value={this.state.zip} onChange={this.handleChange} />
+          <input
+            type="number"
+            id="zip"
+            maxLength="5"
+            name="zip"
+            value={this.state.zip}
+            onChange={this.handleChange}
+            placeHolder="ZIP"
+          />
           <input type="submit" />
         </form>
       </section>
