0e8c670396c84ff2664ebf40548395b40e3e67c5
Jeff Goodall <goodallhj@gmail.com>
2018-02-18 16:22:48 -0700

    Adding Products cleaner to textCard

--- src/components/TextCard/TextCard.js
+++ src/components/TextCard/TextCard.js
@@ -2,32 +2,43 @@ import React, { Component } from 'react';
 import './TextCard.css';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-
-
+import { Link } from 'react-router-dom';
 
 export class TextCard extends Component {
-
   render() {
     const active = this.props.activeMarket;
-    const marketInfo = this.props.markets.find(market=> market.id === active.id);
-    
+    const marketInfo = this.props.markets.find(
+      market => market.id === active.id
+    );
+
+    const products = marketInfo.Products.split(';').map((product, index)=>{
+      return <li key={index}>{product}</li>;
+    });
+
     return (
-      <div className='text-card'>
-        <p>schedule {marketInfo.Schedule}</p>
-        <p>Products {marketInfo.Products}</p>
-        <p>Address {marketInfo.Address}</p>
-        <p>GoogleLink {marketInfo.GoogleLink}</p>
-        <p>marketname {marketInfo.marketname}</p>
+      <div className="text-card">
+        <Link to="/single-market-map" className='link'>Map View</Link>
+        <h2 className='name'>marketname {marketInfo.marketname}</h2>
+        <div className='info'>
+          <p>Address: {marketInfo.Address}</p>
+          <p>Schedule: {marketInfo.Schedule}</p>
+          <ol>Products:
+          {products}
+          </ol>
+        </div>
+        <div id='map'>GoogleLink {marketInfo.GoogleLink}</div>
       </div>
-    )
+    );
   }
 }
 
-export const mapStateToProps = state => ({ markets: state.markets, activeMarket: state.activeMarket });
-
-export const mapDispatchToProps = dispatch => ({
-  
+export const mapStateToProps = state => ({
+  markets: state.markets,
+  activeMarket: state.activeMarket
 });
 
+export const mapDispatchToProps = dispatch => ({});
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TextCard));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(TextCard)
+);
