823f74f37473cab73ba61c307a342b2828179dba
Jeff Goodall <goodallhj@gmail.com>
2018-02-25 15:44:45 -0700

    adding tests to Container.js

--- src/components/Container/Container.test.js
+++ src/components/Container/Container.test.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import Container from './Container';
-import {shallow} from 'enzyme';
+import { Container, mapStateToProps, mapDispatchToProps } from './Container';
+import { shallow } from 'enzyme';
 
 describe('CONTAINER', () => {
   it('should match the snapshot', () => {
-    const wrapper = shallow(<Container/>);
+    const wrapper = shallow(<Container markets={[]} user={[]} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('MSTP and MDTP', () => {
+    it('should define props MARKETS', () => {
+      const mockStore = {
+        markets: [],
+        user: { favorites: [] }
+      };
+      const expected = [];
+      const mapped = mapStateToProps(mockStore);
+      expect(mapped.markets).toEqual(expected);
+    });
+
+    it('should define props USER', () => {
+      const mockStore = {
+        markets: [],
+        user: { favorites: [] }
+      };
+      const expected = [];
+      const mapped = mapStateToProps(mockStore);
+      expect(mapped.user).toEqual(expected);
+    });
+
+    it('should call dispatch on MARKETDETAILS', () => {
+      const mockDispatch = jest.fn();
+      const mapped = mapDispatchToProps(mockDispatch);
+      mapped.marketDetails();
+      expect(mockDispatch).toHaveBeenCalled();
+    });
   });
 });
