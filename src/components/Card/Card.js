d07fc244ee997137e349c282984ce0d9ec121fd7
Jeff Goodall <goodallhj@gmail.com>
2018-02-26 08:17:57 -0700

    have hearts being toggled win win

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import { MapContainer } from '../MapContainer/MapContainer';
+import * as actions from '../../actions/actions';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './Card.css';
 
 export class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      market: {},
+      favorites: []
+    };
+  }
+  componentDidMount() {
+    this.setState({favorites: this.props.user});
+  }
+
+  favorite = (event, market) => {
+    if (market.favorite === false) {
+      market.favorite = true;
+      this.props.updateFavorites(market);
+      this.setState({market});
+    } else {
+      market.favorite = false;
+      this.props.removeFavorite(market);
+      this.setState({market: {}});
+    }
+  };
+
   render() {
-    const { id, markets, fav } = this.props;
+    const { id, markets } = this.props;
     const marketInfo = markets.find(market => market.id === id);
     const schedule = marketInfo.Schedule.slice(0, -16);
     const products = marketInfo.Products.split(';').map((product, index) => {
       return <li key={index}>{product}</li>;
     });
-    const isFavorite = this.props.user.some(userFav => userFav.id === id);
 
     return (
       <div className="text-card">
         <span
-          className={isFavorite ? 'favorite active' : 'favorite'}
-          onClick={event => fav(event, marketInfo)}
+          className={marketInfo.favorite ? 'favorite active' : 'favorite'}
+          onClick={event => this.favorite(event, marketInfo)}
         >
           &#9829;
         </span>
@@ -41,7 +64,9 @@ Card.propTypes = {
   id: PropTypes.string,
   markets: PropTypes.array,
   fav: PropTypes.func,
-  user: PropTypes.array
+  user: PropTypes.array,
+  updateFavorites: PropTypes.func,
+  removeFavorite: PropTypes.func
 };
 
 export const mapStateToProps = state => ({
@@ -49,4 +74,9 @@ export const mapStateToProps = state => ({
   user: state.user.favorites
 });
 
-export default withRouter(connect(mapStateToProps)(Card));
+export const mapDispatchToProps = dispatch => ({
+  updateFavorites: favorite => dispatch(actions.updateFavorites(favorite)),
+  removeFavorite: favorite => dispatch(actions.removeFavorite(favorite))
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Card));
